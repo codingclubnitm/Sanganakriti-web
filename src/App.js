8968245208470c76df4d686cfada2b51c8f968fc
codingclubnitm <codingclub@nitm.ac.in>
2023-12-13 11:52:11 +0530

    initialization

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+        <p >
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+          Learn React
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
