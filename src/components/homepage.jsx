a606bc0d9b23819adf4c6db2588078f7d34ea8be
codingclubnitm <codingclub@nitm.ac.in>
2023-12-14 12:33:29 +0530

    component folder and homepage completed

---
+++ src/components/homepage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Wave from "react-wavify";
+
+const Homepage = () => {
+  return (
+    <div className="text-right bg-background">
+      <div className=" flex flex-col items-center pt-40 mx-auto mb-10">
+        <img
+          src="images/web-image.png"
+          alt="website name"
+          style={{ width: "500px" }}
+        />
+        <p className="text-white pb-8">Coding Club NIT MEGHALAYA</p>
+        <p className="text-white">
+          "Cultivate ideas, spark creativity, and forge a collaborative space
+          where members thrive
+        </p>
+
+        <p className="text-white"> in the world of coding and technology"</p>
+      </div>
+
+      <Wave
+        fill="url(#gradient)"
+        paused={false}
+        style={{ display: "flex" }}
+        options={{
+          height: 20,
+          amplitude: 50,
+          speed: 0.2,
+          points: 7,
+        }}
+      >
+      
+        <defs>
+          <linearGradient id="gradient" gradientTransform="rotate(90)">
+            <stop offset="10%" stopColor="#033B60" />
+            <stop offset="80%" stopColor="#000000" />
+          </linearGradient>
+        </defs>
+      </Wave>
+      <div className="bg-background p-5">
+
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
